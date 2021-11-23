014941e59245fe5553a6dc8b52b83b4e4804a18a
Zhao KK <kikakkz@hotmail.com>
2021-11-23 16:15:21 +0000

    Add payment

---
+++ api/payment.go
@@ -0,0 +1,42 @@
+// +build !codeanalysis
+
+package api
+
+import (
+	"context"
+
+	"github.com/NpoolPlatform/cloud-hashing-order/message/npool"
+	"github.com/NpoolPlatform/cloud-hashing-order/pkg/crud/payment" //nolint
+
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (s *Server) CreatePayment(ctx context.Context, in *npool.CreatePaymentRequest) (*npool.CreatePaymentResponse, error) {
+	resp, err := payment.Create(ctx, in)
+	if err != nil {
+		logger.Sugar().Errorw("create payment error: %w", err)
+		return &npool.CreatePaymentResponse{}, status.Error(codes.Internal, err.Error())
+	}
+	return resp, nil
+}
+
+func (s *Server) UpdatePayment(ctx context.Context, in *npool.UpdatePaymentRequest) (*npool.UpdatePaymentResponse, error) {
+	resp, err := payment.Update(ctx, in)
+	if err != nil {
+		logger.Sugar().Errorw("update payment error: %w", err)
+		return &npool.UpdatePaymentResponse{}, status.Error(codes.Internal, err.Error())
+	}
+	return resp, nil
+}
+
+func (s *Server) GetPaymentByOrder(ctx context.Context, in *npool.GetPaymentByOrderRequest) (*npool.GetPaymentByOrderResponse, error) {
+	resp, err := payment.GetByOrder(ctx, in)
+	if err != nil {
+		logger.Sugar().Errorw("get payment by order error: %w", err)
+		return &npool.GetPaymentByOrderResponse{}, status.Error(codes.Internal, err.Error())
+	}
+	return resp, nil
+}
